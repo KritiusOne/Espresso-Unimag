60e2e10079c8bddcb05106f0a080d2747c5d6518
Julio Rodríguez <julio10489@gmail.com>
2023-10-14 18:26:12 -0500

    cambio en la maquetacion del header para agregar el logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
+import logo from "../../assets/logo.png"
 import "./header.css"
 export function Header() {
   return (
     <header className="header">
+      <div className="header--container">
+        <div className="header__img--container">
+          <img src={logo} alt="" className="header__logo" />
+        </div>
+        <div className="header__img--container">
+          <span className="header__button--SingIn">
+            Registrarse
+          </span>
+        </div>
+      </div>
       <nav className="header__nav">
         <ul className="header__nav__list">
           <div className="header__nav__list--container">
