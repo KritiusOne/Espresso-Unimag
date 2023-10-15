b26a13cb0be598059c1e4dd2775dc3d345841e3d
Julio Rodríguez <julio10489@gmail.com>
2023-10-15 15:31:41 -0500

    Nuevos Mocks

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from "../../components/header/Header.jsx"
 
 export function Inicio() {
   return (
-    <div>Inicio</div>
+    <div>
+      <Header />
+    </div>
   )
 }
--- src/utils/MetaInfo.js
+++
@@ -1,6 +0,0 @@
-export const category = {
-  mecatos: "Mecatos, chicles y galletas",
-  horneados: "Horneados",
-  bebidas: "Gaseosas y Café",
-  personal: "personal"
-}
