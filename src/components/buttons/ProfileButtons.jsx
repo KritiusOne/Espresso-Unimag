639377e500cafada1a3730a67f85dfb4c3c25fe1
Julio Rodríguez <julio10489@gmail.com>
2023-10-22 13:25:14 -0500

    estilos del icono y cambio en la maquetacion

--- src/components/buttons/ProfileButtons.jsx
+++ src/components/buttons/ProfileButtons.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { BsCart4 } from "react-icons/bs"
 import { ProductsContext } from '../../context/productsContext'
+import { Button } from "./Button.jsx"
 import "./profileButton.css"
 
 
@@ -11,9 +12,7 @@ export function ProfileButtons() {
 
   return (
     <div className='profileButtons'>
-      <strong>
-        <BsCart4 className='profileButtons__icon' />
-      </strong>
+      <Button className="profileButtons__icon" Icon={BsCart4} />
       <button className='profileButtons__userButton' >
         <strong className='profileButtons__userButton__title'>
           {
