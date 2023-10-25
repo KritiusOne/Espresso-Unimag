30c6e19d3451a70f8ea051ac1cb91334ad5ef79f
Julio Rodríguez <julio10489@gmail.com>
2023-10-25 11:32:19 -0500

    Agregado: Estilos para el componente modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AiOutlineCloseCircle } from "react-icons/ai"
+import { InicioContext } from '../../Pages/Inicio/InicioContext'
 import "./modal.css"
 
 export function Modal({ children }) {
+  const { setViewModal } = useContext(InicioContext)
+  const handleClickCloseModal = () => {
+    setViewModal(false)
+  }
   return (
     <section className='modalOverlay'>
       <section className='modal'>
-        {
-          children
-        }
+        <AiOutlineCloseCircle className="modal__icon" onClick={handleClickCloseModal} />
+        <main className='modal__main'>
+          {
+            children
+          }
+        </main>
       </section>
     </section>
   )
