ab0306d52687fc5a892c393c68808849085dfe7c
Julio Rodríguez <julio10489@gmail.com>
2023-10-26 11:44:01 -0500

    Añadido: Maquetacion de la pagina Preguntas frecuentes

--- src/Pages/preguntas/Preguntas.jsx
+++ src/Pages/preguntas/Preguntas.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import { LayoutPage } from "../../components/layout/LayoutPage"
+import { Footer } from "../../components/footer/Footer.jsx"
+import img from "../../assets/vendedor.jpg"
+import "./preguntas.css"
 
 export function Preguntas() {
   return (
-    <LayoutPage>Preguntas</LayoutPage>
+    <LayoutPage className="Preguntas">
+      <main className='Preguntas__main'>
+        <section className='Preguntas__main__content'>
+          <h6 className="Preguntas__main__content__pregunta">pregunta 1</h6>
+          <h6 className="Preguntas__main__content__pregunta">pregunta 2</h6>
+          <h6 className="Preguntas__main__content__pregunta">pregunta 3</h6>
+          <h6 className="Preguntas__main__content__pregunta">pregunta 4</h6>
+          <h6 className="Preguntas__main__content__pregunta">pregunta 5</h6>
+        </section>
+        <aside className='Preguntas__main__aside'>
+          <img src={img} className='Preguntas__main__aside__img' alt="Esta es una representacion del documento que debe ir aquí" />
+        </aside>
+      </main>
+      <Footer />
+    </LayoutPage>
   )
 }
