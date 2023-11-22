a4b6d59f3a955f05f1dcdd23ec437ecaf6c15a23
Julio Rodríguez <julio10489@gmail.com>
2023-11-22 18:50:08 -0500

    Agregado: Renderizado de inicio condicional segun el rol que desempeña el usuario, pendiente por estilos

--- src/Pages/inicioRegistro/InicioRegistro.jsx
+++ src/Pages/inicioRegistro/InicioRegistro.jsx
@@ -3,16 +3,18 @@ import { LayoutPage } from '../../components/layout/LayoutPage'
 import { RolesTypes } from '../../utils/RolesTypes'
 import { CardTypeUser } from '../../components/cards/CardTypeUser'
 import "./inicioRegistro.css"
-
+import { useAuth } from '../../context/authContext/AuthContext'
+import { UserLogged } from '../../components/inicioUsersLogged/UserLogged'
 export function InicioRegistro() {
+  const { currentUser } = useAuth()
   const [posiblesRoles, setPosiblesRoles] = useState(Object.values(RolesTypes))
   return (
     <LayoutPage>
       <main className='main' >
         {
-          posiblesRoles.map((role) => {
+          currentUser == null ? posiblesRoles.map((role) => {
             return <CardTypeUser className={`${role}`} namecard={role} key={role} />
-          })
+          }) : <UserLogged />
         }
       </main>
     </LayoutPage>
--- src/Pages/user/UserProfile.jsx
+++ src/Pages/user/UserProfile.jsx
@@ -4,17 +4,14 @@ import { Button } from '../../components/buttons/Button'
 import { useAuth } from '../../context/authContext/AuthContext'
 import { ProductsContext } from '../../context/productsContext'
 import { RolesTypes } from '../../utils/RolesTypes'
-import { OPTIONS_FROM_VENDEDOR, OPTIONS_FROM_ADMIN, OPTIONS_FROM_CLIENTE } from '../../utils/UserOptionTypes'
+import { OPTIONS_FROM_CLIENTE } from '../../utils/UserOptionTypes'
 import Card from '../../components/cards/CardOptionProfile'
 import "./userProfile.css"
 import { useNavigate } from 'react-router'
 import { TypesRoutes } from '../../routes/TypesRoutes'
 
 export function UserProfile() {
-  const [isCliente, setIsCliente] = useState(false)
-  const [isVendedor, setIsVendedor] = useState(false)
-  const [isAdmin, setIsAdmin] = useState(false)
-  const { rol, logout } = useAuth()
+  const { logout } = useAuth()
   const { deleteAllProductsOnCart } = useContext(ProductsContext)
   const navegation = useNavigate()
   const fnLogOut = () => {
@@ -22,35 +19,16 @@ export function UserProfile() {
     logout()
     navegation(TypesRoutes.HOME)
   }
-  useEffect(() => {
-    if (rol == RolesTypes.ADMINISTRADOR) setIsAdmin(true)
-    if (rol == RolesTypes.CLIENTE) setIsCliente(true)
-    if (rol == RolesTypes.VENDEDOR) setIsVendedor(true)
-  }, [])
   return (
     <LayoutPage>
       <main className='UserProfile'>
-        {
-          isCliente && <section className='UserProfile__SectionOptionProfile'>
-            {
-              OPTIONS_FROM_CLIENTE.map(option => <Card Icon={option.Icon} namecard={option.title} fn={() => console.log("FUNCIONO")} />)
-            }
-          </section>
-        }
-        {
-          isAdmin && <section className='UserProfile__SectionOptionProfile'>
-            {
-              OPTIONS_FROM_ADMIN.map(option => <Card Icon={option.Icon} namecard={option.title} fn={() => console.log("FUNCIONO")} />)
-            }
-          </section>
-        }
-        {
-          isVendedor && <section className='UserProfile__SectionOptionProfile'>
-            {
-              OPTIONS_FROM_VENDEDOR.map(option => <Card Icon={option.Icon} namecard={option.title} fn={() => console.log("FUNCIONO")} />)
-            }
-          </section>
-        }
+
+        <section className='UserProfile__SectionOptionProfile'>
+          {
+            OPTIONS_FROM_CLIENTE.map(option => <Card Icon={option.Icon} namecard={option.title} fn={() => console.log("FUNCIONO")} />)
+          }
+        </section>
+
         <Button title='Cerrar sesion' clickHandler={fnLogOut} />
       </main>
     </LayoutPage>
